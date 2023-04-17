68b6a0dd5f571e262d9b540a3efea6dbb6b1519d
Eric Harris-Braun <eric@harris-braun.com>
2023-04-17 14:25:00 -0400

    add nickname when displaying avatar

--- ui/src/emergence/emergence/Avatar.svelte
+++ ui/src/emergence/emergence/Avatar.svelte
@@ -4,15 +4,28 @@
   import { storeContext } from '../../contexts';
   import type { EmergenceStore } from '../../emergence-store';
   import { getContext } from "svelte";
+  import type { ProfilesStore } from "@holochain-open-dev/profiles";
 
   let store: EmergenceStore = (getContext(storeContext) as any).getStore();
+  let profilesStore: ProfilesStore = store.profilesStore
 
   export let agentPubKey: AgentPubKey
   export let size = 32
 
-  $:agentPubKey
-
+  $: agentPubKey
+  $: s = store.profilesStore.profiles.get(agentPubKey)
+  $: nickname = $s.status == "complete" ? $s.value?.nickname : "..."
 </script>
 
+<div class="avatar">
 <agent-avatar size={size} agent-pub-key="{encodeHashToBase64(agentPubKey)}"></agent-avatar>
-<!-- nickname: {JSON.stringify(store.profilesStore.profiles.get(agentPubKey))} -->
+{nickname}
+</div>
+
+<style>
+    .avatar {
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
