ab3a321d56585e15e6625758a2e78bcc23cd20a6
Jordan <starpause@gmail.com>
2023-04-20 10:42:52 -0700

    hotrealoading css for windows

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,5 +4,13 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  server: {
+    hmr: {
+        host: 'localhost',
+    },
+    watch: {
+        usePolling: true
+    }
+  },
 });
 
