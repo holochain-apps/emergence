224bb7126494a3a1f9a04d2f7e3a98d43feb07fd
Jost Schulte <jost.schulte@protonmail.com>
2023-05-16 19:13:01 -0600

    build: fix nix flake

--- flake.nix
+++ flake.nix
@@ -4,10 +4,10 @@
   inputs = {
     nixpkgs.follows = "holochain/nixpkgs";
 
+    versions.url = "github:holochain/holochain?dir=versions/0_1";
     holochain = {
       url = "github:holochain/holochain";
-      inputs.versions.url = "github:holochain/holochain/?dir=versions/0_1";
-      inputs.holochain.url = "github:holochain/holochain/holochain-0.1.3";
+      inputs.versions.follows = "versions";
     };
   };
 
