441c4c5c9722f180462f37ef66166fd11102b296
Eric Harris-Braun <eric@harris-braun.com>
2023-05-18 09:09:55 -0400

    add missing SpaceLink file

---
+++ ui/src/emergence/emergence/SpaceLink.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+   import type { ActionHash } from '@holochain/client';
+  import type { EmergenceStore } from '../../emergence-store';
+  import { getContext, onMount } from 'svelte';
+  import { storeContext } from '../../contexts';
+
+  let store: EmergenceStore = (getContext(storeContext) as any).getStore();
+  $: uiProps = store.uiProps
+
+  export let spaceHash: ActionHash;
+  export let linkText = ""
+
+  onMount(() => {
+  });
+
+  const handleClick = ()=> {
+    store.setUIprops({spaceDetails:spaceHash})
+  }
+  const spaceTitle = (spaceHash: ActionHash) => {
+    const space = store.getSpace(spaceHash)
+    if (space) {
+      return space.record.entry.name
+    }
+    return "<deleted space>"
+  }
+</script>
+
+<div class="space-link" on:click={handleClick}>{linkText ? linkText : spaceTitle(spaceHash)}</div>
+
+<style>
+    .space-link {
+      margin: 0 5px 0 5px;
+      cursor: pointer;
+      text-decoration: underline;
+      color: blue;
+    }
+</style>
