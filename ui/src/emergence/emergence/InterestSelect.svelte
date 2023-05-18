bea1a740098ad037588d9d2680b8a0256dd036f0
Eric Harris-Braun <eric@harris-braun.com>
2023-05-18 16:47:03 -0400

    convert interest to buttons

--- ui/src/emergence/emergence/InterestSelect.svelte
+++ ui/src/emergence/emergence/InterestSelect.svelte
@@ -8,7 +8,7 @@ import '@shoelace-style/shoelace/dist/components/menu-item/menu-item.js';
 import { SessionInterest } from './types';
 import { storeContext } from '../../contexts';
 import type { EmergenceStore } from '../../emergence-store';
-import {  faBookmark, faChevronDown, faEnvelope, faStar } from '@fortawesome/free-solid-svg-icons';
+import {  faBookmark, faCheck, faChevronDown, faEnvelope, faStar } from '@fortawesome/free-solid-svg-icons';
 import type{  ActionHash } from '@holochain/client';
 import type { Snackbar } from '@material/mwc-snackbar';
 import Fa from 'svelte-fa';
@@ -22,10 +22,12 @@ $: session = store.sessionStore(sessionHash)
 $: relData = store.sessionReleationDataStore(session)
 
 async function setSessionInterest(interest: SessionInterest) {
-  open=false
   try {
     if (interest !== $relData.myInterest)
       await store.setSessionInterest($session.original_hash, interest )
+    else 
+      await store.setSessionInterest($session.original_hash, SessionInterest.NoOpinion )
+
   } catch (e: any) {
     console.log("SET SESSION INTEREST ERROR", e)
 
@@ -33,59 +35,47 @@ async function setSessionInterest(interest: SessionInterest) {
     errorSnackbar.show();
   }
 }
-let open = false
-let menu
 </script>
 <mwc-snackbar bind:this={errorSnackbar} leading>
 </mwc-snackbar>
-  <div class="select" on:mousedown={(e)=>{open = true;e.stopPropagation()}}
-    >
-  <div style="margin-right: 5px">
-    {#if $relData.myInterest === SessionInterest.NoOpinion}RSVP{/if}
-    {#if $relData.myInterest === SessionInterest.Going}<Fa icon={faStar} />{/if}
-    {#if $relData.myInterest === SessionInterest.Interested}<Fa icon={faBookmark} />{/if}
-  </div>
-  <Fa icon={faChevronDown} />
-  {#if open}
-    <div class="menu">  
-      <sl-menu 
-      bind:this={menu}
-      value={`${$relData.myInterest}`}
-      on:mouseleave={(e)=>{ 
-        if (e.target == menu) {
-          open = false;e.stopPropagation()
-        }
-        }}
 
-      on:sl-select={(e) =>   {e.stopPropagation(); setSessionInterest(parseInt(e.detail.item.value)) }}
-      >
-        <sl-menu-item value={SessionInterest.NoOpinion}>RSVP</sl-menu-item>
-        <sl-menu-item value={SessionInterest.Going}><Fa slot="prefix" icon={faStar} />Going</sl-menu-item>
-        <sl-menu-item value={SessionInterest.Interested}><Fa slot="prefix" icon={faBookmark} /> Interested</sl-menu-item>
-      </sl-menu>
-    </div>
-  {/if}
+<div class="interest">
+  <div class="interest-button" style="margin-right:5px"
+    title="I'm Interested"
+    class:selected={$relData.myInterest == SessionInterest.Interested}
+    on:click={() => {setSessionInterest(SessionInterest.Interested)}} >
+    <div ><Fa icon={ faBookmark } /></div>
   </div>
+  <div class="interest-button"  
+    title="I'm Going!"
+    class:selected={$relData.myInterest == SessionInterest.Going}
+    on:click={() => {setSessionInterest(SessionInterest.Going)}} >
+    <div ><Fa icon={ faCheck } /></div>
+  </div>
+</div>
+
 
 <style>
-  .select {
-    position: relative;
+  .interest {
     display:flex;
-    border: 2px solid rgba(70, 47, 216, .20);
-    padding: 0 10px;
-    font-size: 14px;
-    border-radius: 5px;
-    align-items: center;
-    color: #462FD8;
-    font-weight: bold;
-    height: 35px;
-    z-index: 0;
+    flex-direction: row;
   }
-  .menu {
-    background-color: white;
-    position: absolute;
-    top: 0;
-    right: 0;   
-    z-index: 1000;
+  .interest-button {
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    width:30px;
+    height:30px;
+    border-radius: 50%;
+    border: solid 1px rgb(203, 203, 203);
+
+  }
+  .selected {
+    outline: none;
+    border-color: #9ecaed;
+    box-shadow: 0 0 10px #9ecaed !important;
+    background-color: rgb(240, 249, 2244);
+    border: solid 1px rgb(149, 219, 252);
+
   }
 </style>
