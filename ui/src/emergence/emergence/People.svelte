a54c5307812e90791050fa10e657a8deb91549b5
Eric Harris-Braun <eric@harris-braun.com>
2023-06-13 20:29:10 -0400

    Improvements to people tab in Discover

--- ui/src/emergence/emergence/People.svelte
+++ ui/src/emergence/emergence/People.svelte
@@ -8,7 +8,7 @@
   import { encodeHashToBase64 } from '@holochain/client';
   import "@holochain-open-dev/profiles/dist/elements/agent-avatar.js";
   import "@holochain-open-dev/file-storage/dist/elements/show-image.js";
-  import { DetailsType, type Info, type ProxyAgent } from './types';
+  import { DetailsType, sessionInterestToString, type Info, type ProxyAgent } from './types';
   import { faInfoCircle, faSearch } from '@fortawesome/free-solid-svg-icons';
   import Fa from 'svelte-fa';
 
@@ -23,7 +23,8 @@
   $: proxyAgents = store.proxyAgents
   $: people = filteredPeople(allPeople, $proxyAgents, $uiProps)
   $: error;
-  
+  $: allSessions = store.sessions;
+
   const filteredPeople = (allPeople, proxyAgents: Array<Info<ProxyAgent>>, uiProps) => {
     const detailedPeople = allPeople.map(([key,profile])=> {
       return {type:'Agent', hash: key, nickname: profile.nickname, bio: profile.fields.bio, location: profile.fields.location}
@@ -76,23 +77,25 @@
 
   <div class="people">
     {#each people as {type, hash, bio, location, nickname, avatarImage}}
-      <div class="person card"
+      <div class="person card clickable"
       on:click={(e)=>{
         store.openDetails(type == "ProxyAgent" ? DetailsType.ProxyAgent : DetailsType.Folk, hash)
         e.stopPropagation()
     }}    >
         <div class="details">
           {#if type == "ProxyAgent"}
-            {#if avatarImage}
-              <show-image style={`width:50px`} image-hash={encodeHashToBase64(avatarImage)}></show-image>
-            {:else}
-              <holo-identicon disable-tooltip={true} disable-copy={true} size={50} hash={hash}></holo-identicon>
-            {/if}
+            <div style="margin-right:10px">
+              {#if avatarImage}
+                <show-image style={`width:50px`} image-hash={encodeHashToBase64(avatarImage)}></show-image>
+              {:else}
+                <holo-identicon disable-tooltip={true} disable-copy={true} size={50} hash={hash}></holo-identicon>
+              {/if}
+            </div>
           {:else}
               <agent-avatar disable-tooltip={true} disable-copy={true} size={50} agent-pub-key="{encodeHashToBase64(hash)}"></agent-avatar>
               
             <div class="info">
-              <div class="nuame">{nickname}</div>
+              <div class="name">{nickname}</div>
               <div class="location">{#if location}<span>{location}</span>{/if}</div>
             </div>
           {/if}
@@ -101,17 +104,32 @@
               <div slot="content" style="color:white">
                 This person is a proxy agent, i.e. they don't have an account on the system and were added by administrators, likely because they are a session leader.
               </div>
-              <span style="display:flex; align-items:center" title="Proxy agent">{nickname} <Fa style="margin-left:3px" icon={faInfoCircle}/></span>
+              <span style="display:flex; align-items:center; font-weight:bold" title="Proxy agent">{nickname} <Fa style="margin-left:5px" icon={faInfoCircle}/></span>
             </sl-tooltip>
           {/if}
         </div>
-        <div style="display:flex;flex-direction:row;align-items: left;margin-left:20px">
-          {#each $agentSessions.get(hash) ? Array.from($agentSessions.get(hash)):  [] as [session, interest] }
-          {#if !store.getSession(session)} &lt;unknown Session&gt;
-          {:else}
-            <div style="margin-left:5px">{ store.getSession(session).record.entry.title}, </div>
-            {/if}
-          {/each}
+        <div style="display:flex;flex-direction:row;align-items: left;margin-left:20px;">
+          <!-- {#if type== "ProxyAgent"} -->
+            <span style="font-weight:strong">Hosting:</span>
+            {#each $allSessions.filter((s) =>
+              s.record.entry.leaders.find(
+                (l) =>
+                  encodeHashToBase64(l.hash) ==
+                  encodeHashToBase64(hash)
+              )
+            ) as session}
+             <div style="margin-left:5px">{ session.record.entry.title} </div>
+
+            {/each}
+
+          <!-- {:else}
+            {#each $agentSessions.get(hash) ? Array.from($agentSessions.get(hash)):  [] as [session, interest] }
+              {#if !store.getSession(session)} &lt;unknown Session&gt;
+              {:else}
+                <div style="margin-left:5px">{sessionInterestToString(interest)}: { store.getSession(session).record.entry.title}, </div>
+              {/if}
+            {/each}
+          {/if} -->
         </div>
         </div>
     {/each}
@@ -125,7 +143,7 @@
     padding: 10px;
     width:100%; 
     display: flex;
-    justify-content: left;
+    justify-content: space-between;
   }
 
   .details {
